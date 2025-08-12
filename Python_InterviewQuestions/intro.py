b53ee3c95902432814e899369a079890ca9f8093
Adil <mohamedadilm23@example.com>
2025-08-12 13:23:18 +0530

    newly committed da

---
+++ Python_InterviewQuestions/intro.py
@@ -0,0 +1,7 @@
+"""Hi python how are you"""
+print("Hello world")
+# This is a single line comment in Python
+# Python supports single-line comments using the hash symbol (#)
+# and multi-line comments using triple quotes (''' or """).
+# This is a multi-line comment in Python
+"""Python is a high-level, interpreted programming language known for its simplicity and readability."""
